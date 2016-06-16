1296eaff6cd1c021230bcce65db6538a03257d8a
Omar <omar_hajeer@hotmail.com>
2016-06-16 17:47:00 +0300

    sdsdsd

--- src/data/models/data2.py
+++ src/data/models/data2.py
@@ -7,6 +7,9 @@ def find_p(n,v, les=None):
     a=filter(lambda x: x.name==n and x.value==v,ps)
     return map(lambda x: x.e,a)
 
+def get_p1(name,e):
+    return filter(lambda x: x.name==name,e.p)[0].getValue()
+    
 def find_class(cls,les=None):
     find_p('class',cls,les)
 
@@ -29,6 +32,9 @@ class P():
     
     def getVlaue(self):
         return self.value
+    
+    def add_p(self,name,value):
+        P(self, name,value)
 
 class R():
     def __init__(self,name):
@@ -50,10 +56,15 @@ def account_move():
     find_class('account-move')
 def create_account_move(move):
     a=find_class('account')
-    frm = filter(lambda x: x.name=='from',move.p)[0].getValue()
-    to = filter(lambda x: x.name=='to',move.p)[0].getValue()
+    frm = get_p1('from',move)
+    to = get_p1('to',move)
+    amount = get_p1('amount',move)
     frm_account=find_p('name',frm,a)
     to_account=find_p('name',to,a)
+    
+    #modify debt,credit of frm and to accounts
+    frm_account.add_p('debt', amount)
+    to_account.add_p('credit', amount)
 
 z = onevent('create',account_move)
 
